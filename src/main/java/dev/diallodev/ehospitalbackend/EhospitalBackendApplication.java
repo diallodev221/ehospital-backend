53b5614a39ef4790684a6a42315dd3ed04f995cb
Saïfoulaye <diallocodeur@gmail.com>
2023-08-06 09:54:34 +0000

    add the project structure

---
+++ src/main/java/dev/diallodev/ehospitalbackend/EhospitalBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.diallodev.ehospitalbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EhospitalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EhospitalBackendApplication.class, args);
+	}
+
+}
